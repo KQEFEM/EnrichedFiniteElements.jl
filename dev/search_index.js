8cdb3d8b5ce33c4acd329489bb1f17972efba2a8
Documenter.jl <documenter@juliadocs.github.io>
2025-02-03 14:28:48 +0000

    build based on db5711a

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = EnrichedFiniteElements","category":"page"},{"location":"#EnrichedFiniteElements","page":"Home","title":"EnrichedFiniteElements","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for EnrichedFiniteElements.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [EnrichedFiniteElements]","category":"page"}]
+}
