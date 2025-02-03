9baca06723696dec07808416f0134b9b1e0bfc8b
Kieran <34337935+KieranSQ@users.noreply.github.com>
2025-02-03 19:06:34 +0100

    Fixed bug

--- src/Operators.jl
+++ src/Operators.jl
@@ -6,22 +6,39 @@ import ..IntegrationWrappers as help
 
 export all
 
-function pDtq(upper_bounds::Vector{Float64}, lower_bounds::Vector{Float64}, A_val::Vector{Float64}, B_val::Vector{Float64}, C_val::Vector{Float64}, omega::Vector{Float64})    
+function pDtq(upper_bounds::Vector{Float64}, lower_bounds::Vector{Float64}, A_val::Float64, B_val::Float64, C_val::Float64, omega::Vector{Float64}) 
+    
     function integrand(v)
         x = v[1]
         y = v[2]
         z = v[3]
-        space_result_ansatz = help.space_enrichment_wrapper([x, y, z], basis.e_space, A_val[1], B_val[1], C_val[1]) # Pass vector, call e_space
-        time_result_ansatz = help.time_enrichment_wrapper([x, y, z], basis.e_time_ansatz, omega[1]) # Pass vector, call e_time_ansatz
-        hat_result_ansatz = help.hat_wrapper([x, y, z], basis.phi) # Pass vector, call hat_function
-        space_result_test = help.space_enrichment_wrapper([x, y, z], basis.e_space, A_val[2], B_val[2], C_val[2]) # Pass vector, call e_space
-        time_result_test = help.time_enrichment_wrapper([x, y, z], basis.e_time_ansatz, omega[2]) # Pass vector, call e_time_ansatz
-        hat_result_test = help.hat_wrapper([x, y, z], basis.phi) # Pass vector, call hat_function
+        space_enrichment = help.space_enrichment_wrapper([x, y, z], basis.enrich_space, A_val, B_val, C_val) # Pass vector, call enrich_space
+        time_enrichment = help.time_enrichment_wrapper([x, y, z], basis.enrichment_time, diff(omega)[1]) # Pass vector, call enrichment_time
+        hat_ansatz = help.hat_wrapper([x, y, z], basis.phi) # Pass vector, call hat_function
+        hat_test = help.hat_wrapper([x, y, z], basis.phi) # Pass vector, call hat_function
 
-        return space_result_ansatz * time_result_ansatz * hat_result_ansatz * conj(omega[2]space_result_test * time_result_test * hat_result_test')  # Multiply the RESULTS
+        return space_enrichment * time_enrichment * hat_ansatz * (omega[2]  * hat_test')  # Multiply the RESULTS
     end
     integral_result = hcubature(integrand, lower_bounds, upper_bounds) # Use integrand
     return integral_result
 end
 
+    function v_nabla_q(upper_bounds::Vector{Float64}, lower_bounds::Vector{Float64}, A_val::Float64, B_val::Float64, C_val::Float64, omega::Vector{Float64}) 
+    """ This is the term \\int_Omega v \\cdot \\nabla conj(q) and equivalent """
+        function integrand(v)
+            x = v[1]
+            y = v[2]
+            z = v[3]
+            space_enrichment = help.space_enrichment_wrapper([x, y, z], basis.enrich_space, A_val, B_val, C_val) # Pass vector, call enrich_space
+            time_enrichment = help.time_enrichment_wrapper([x, y, z], basis.enrichment_time, diff(omega)[1]) # Pass vector, call enrichment_time
+            hat_ansatz = help.hat_wrapper([x, y, z], basis.phi) # Pass vector, call hat_function
+            hat_test = help.hat_wrapper([x, y, z], basis.phi) # Pass vector, call hat_function
+            grads = help.grads_wrapper([x,y,z], basis.grads_matrix)
+
+            return (hat_ansatz .* grads - 1im * hat_ansatz * hat_test' * [k_x, k_y]) * space_enrichment * time_enrichment
+        end
+        integral_result = hcubature(integrand, lower_bounds, upper_bounds) # Use integrand
+        return integral_result
+
+    end 
 end # End of module
