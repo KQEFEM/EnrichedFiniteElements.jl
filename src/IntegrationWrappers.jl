41a50e36b760f57a2025d5b74a603164fc07b9ca
Kieran <34337935+KieranSQ@users.noreply.github.com>
2025-02-03 18:17:47 +0100

    integration wrappers built

---
+++ src/IntegrationWrappers.jl
@@ -0,0 +1,39 @@
+module IntegrationWrappers
+using ..BasisFunctions  # Or import BasisFunctions as basis
+
+export all
+
+function time_enrichment_wrapper(v,f,omega)
+    x = v[1]
+    y = v[2]
+    z = v[3]
+    return f(x,y,z,omega)
+end
+
+function space_enrichment_wrapper(v, f, A_val, B_val, C_val)
+    x = v[1]
+    y = v[2]
+    z = v[3]
+    return f(x, y, z, A_val, B_val, C_val)  # Call your original function
+end
+function hat_wrapper(v,f)
+    x = v[1]
+    y = v[2]
+    z = v[3]
+    return f(x,y,z)
+end 
+
+function e_space_wrap(v)
+    return space_enrichment_wrapper(v, BasisFunctions.e_space ,A_val, B_val, C_val) # Or helper.space_enrichment_wrapper if aliased
+end
+
+function e_time_wrap(v)
+    return time_enrichment_wrapper(v, BasisFunctions.e_time_ansatz, omega)
+end 
+
+function hat_wrap(v)
+    return hat_wrapper(v, BasisFunctions.p_mat)
+end
+
+
+end 
