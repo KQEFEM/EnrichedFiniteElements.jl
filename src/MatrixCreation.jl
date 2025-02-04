acf7db5466968f18eb8bb23f5722d06063bbe1a0
Kieran <34337935+KieranSQ@users.noreply.github.com>
2025-02-04 15:10:51 +0100

    init functions

---
+++ src/MatrixCreation.jl
@@ -0,0 +1,15 @@
+module MatrixCreation
+
+using HCubature
+import ..BasisFunctions as basis
+import ..Operators as op
+
+function mass_matrix(nodes, connectivity, time, wavenumbers)
+    
+
+end 
+
+
+
+
+end 
