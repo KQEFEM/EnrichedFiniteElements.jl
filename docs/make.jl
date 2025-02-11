d44f7cbf2ed12d73d800727bcb83df5fcae5e10b
Kieran <34337935+KieranSQ@users.noreply.github.com>
2025-02-11 16:40:29 +0100

    removed docmeta

--- docs/make.jl
+++ docs/make.jl
@@ -1,22 +1,16 @@
 using EnrichedFiniteElements
 using Documenter
-using DocMeta
-
-# Set up the doctest environment
-DocMeta.setdocmeta!(
-    EnrichedFiniteElements,
-    :DocTestSetup,
-    :(using EnrichedFiniteElements);
-    recursive = true,
-)
 
 # Build the documentation (Markdown format)
 makedocs(
-    format = Documenter.Markdown(),
-    modules = [EnrichedFiniteElements],
-    authors = "Kieran Quaine",
-    pages = ["Home" => "index.md"],
+    modules = [EnrichedFiniteElements],  # Include your module
+    authors = "Kieran Quaine",           # Specify the author
+    format = Documenter.Markdown(),      # Use Markdown format
+    pages = ["Home" => "index.md"],      # Define the pages
 )
 
 # Deploy the docs (ensure you have a GITHUB_TOKEN set for this to work)
-deploydocs(; repo = "github.com/KQEFEM/EnrichedFiniteElements.jl", devbranch = "main")
+deploydocs(
+    repo = "github.com/KQEFEM/EnrichedFiniteElements.jl",  # Repository URL
+    devbranch = "main",                                    # Default branch
+)
