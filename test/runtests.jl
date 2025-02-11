ae65c4da8fc4c68d0af6a3d2ff2819284a67a738
Kieran <34337935+KieranSQ@users.noreply.github.com>
2025-02-11 15:32:23 +0100

    updated runtest

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,5 +2,7 @@ using EnrichedFiniteElements
 using Test
 
 @testset "EnrichedFiniteElements.jl" begin
-    # Write your tests here.
-end
+        include("BasisFunctionsTests.jl")   # Include the tests for BasisFunctions
+        include("EnrichmentCreatorTests.jl") # Include the tests for EnrichmentCreator
+        include("OperatorsTests.jl")      # Include the tests for Operatorsend
+end 
