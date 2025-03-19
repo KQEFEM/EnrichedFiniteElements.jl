2e467d0cb0fc7dfd3f3291e90dc840e0c436d093
Kieran <34337935+KieranSQ@users.noreply.github.com>
2025-03-19 16:33:56 +0100

    updated in a basic case

--- src/MatrixCreation.jl
+++ src/MatrixCreation.jl
@@ -3,11 +3,12 @@ module MatrixCreation
 using HCubature
 import ..BasisFunctions as basis
 import ..Operators as op
+import ..transformationFunctions as Transformations
 
-const Transformations = EnrichedFiniteElements.transformationFunctions
-const integrator = EnrichedFiniteElements.Operators
+# const Transformations = .transformationFunctions
+const integrator = op
 
-function matrix_creation(all_pairs,nodes)
+function spares_matrix_creation(all_pairs,nodes)
     """
     Creates the sparse array needed fro the enriched matrix computations 
     """
@@ -21,15 +22,15 @@ function matrix_creation(all_pairs,nodes)
     return cell_sparse_zero_array
 end 
 
-function mass_matrix(
-    nodes::Matrix{Float64},
-    connectivity::Matrix{Float64},
-    time,
-    wavenumbers::Matrix{Float64} = [0, 0, 0],
-)
+# function mass_matrix(
+#     nodes::Matrix{Float64},
+#     connectivity::Matrix{Float64},
+#     time,
+#     wavenumbers::Matrix{Float64} = [0, 0, 0],
+# )
 
 
-end
+# end
 
 
 
@@ -40,6 +41,8 @@ function compute_sparse_mass_matrix(all_pairs, nodes, result, wavenumbers_ansatz
     cell_sparse_zero_array = matrix_creation(all_pairs,nodes);
 
     for ii in result
+        cell_idx = ii[1][1] # this grabs the tuple ( - , - )
+
         wave_ansatz_loc = wavenumbers_ansatz[ii[1][1][1], :]
         wave_test_loc = wavenumbers_test[ii[1][1][2], :]
         
@@ -51,6 +54,8 @@ function compute_sparse_mass_matrix(all_pairs, nodes, result, wavenumbers_ansatz
         
         kx_kkx = wave_ansatz_loc[1] - wave_test_loc[1]
         ky_kky = wave_ansatz_loc[2] - wave_test_loc[2]
+        omega = [wave_ansatz_loc[3],wave_test_loc[3]]
+
         
         A = kx_kkx * (triangle_nodes[2, 1] - triangle_nodes[1, 1]) +
             ky_kky * (triangle_nodes[2, 2] - triangle_nodes[1, 2])
@@ -64,8 +69,8 @@ function compute_sparse_mass_matrix(all_pairs, nodes, result, wavenumbers_ansatz
         
         upper_bounds = [1.0, 1.0, 1.0]
         lower_bounds = [-1.0, -1.0, 0.0]
-        omega = [0.0, 0.0]
-        dt = 0.1
+
+        dt = 0.1 #! This needs to be in the function input
         t_jump = 0.0
         t0 = 0.0
 
@@ -73,8 +78,47 @@ function compute_sparse_mass_matrix(all_pairs, nodes, result, wavenumbers_ansatz
             upper_bounds, lower_bounds, A, B, C, omega, t_jump, dt, t0, tri_area
         )
         
-        cell_sparse_zero_array[1, 1][triangle_connectivity, triangle_connectivity] .+= mass_loc
+        cell_sparse_zero_array[cell_idx[1], cell_idx[2]][triangle_connectivity, triangle_connectivity] .+=
+        mass_loc
     end
     
     return cell_sparse_zero_array
 end
+
+
+nodes = [ 0.0       0.0
+1.0       0.0
+1.0       1.0
+0.0       1.0
+0.5       0.0
+1.0       0.5
+0.5       1.0
+0.0       0.5
+0.29375   0.70625
+0.375     0.375
+0.647917  0.64375
+0.71875   0.28125]
+connectivity = [  6   3  11
+8   1  10
+1   5  10
+3   7  11
+5   2  12
+2   6  12
+7   4   9
+4   8   9
+6  11  12
+9   8  10
+9  10  11
+10   5  12
+11  10  12
+7   9  11]
+
+
+x_enrichments= 1
+y_enrichments = 1
+wavenumbers = reshape(collect(
+        Iterators.product(-x_enrichments:x_enrichments, -y_enrichments:y_enrichments),
+    ),:,1)
+
+
+    flattened_matrix = hcat(first.(wavenumbers), last.(wavenumbers))
