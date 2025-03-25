e77cd447192bb6a52a64af7c855af90902029a61
Kieran <34337935+KieranSQ@users.noreply.github.com>
2025-03-25 01:41:45 +0100

    init example convection matrix

---
+++ scripts/exampleConvectionMatrix.jl
@@ -0,0 +1,54 @@
+
+using Revise
+using LinearAlgebra  # Import the LinearAlgebra module
+using SparseArrays
+using EnrichedFiniteElements
+
+const Transformations = EnrichedFiniteElements.TransformationFunctions
+const integrator = EnrichedFiniteElements.Operators
+
+const matrix_comp = EnrichedFiniteElements.MatrixCreation
+
+const mesh_create = EnrichedFiniteElements.MeshCreation
+const wave_func = EnrichedFiniteElements.EnrichmentCreator
+
+#! Set up for the matrix creation
+domain = ((0, 1), (0, 1))
+num_nodes = 10
+
+# Create the mesh
+mesh = mesh_create.rectangle_domain(domain)
+
+nodes = mesh.nodes
+connectivity = mesh.connectivity
+boundary_index = mesh.boundary_idx
+boundary_edges = mesh.boundary_edges
+
+wave_x = 0
+wave_y = 0
+ansatz_wave = wave_func.create_wavenumbers(wave_x, wave_y)
+test_ansatz = wave_func.create_wavenumbers(wave_x, wave_y)
+wavenumbers_ansatz, wavenumbers_test =
+    wave_func.wavenumber_creation(ansatz_wave, test_ansatz, 2)
+
+idx_wave_ansatz = collect(1:size(wavenumbers_ansatz, 1))
+idx_wave_test = collect(1:size(wavenumbers_test, 1))
+
+all_pairs = wave_func.generate_pairs(idx_wave_ansatz, idx_wave_test)
+println(all_pairs)
+idx_connectivity = collect(1:size(connectivity, 1))
+result = wave_func.combine_wavenumber_with_all_nodes(all_pairs, connectivity)
+
+wave_node_pairs = result
+dt = 0.1
+
+compute_sparse_matrix(
+    all_pairs,
+    nodes,
+    connectivity_matrix,
+    wavenumbers_ansatz,
+    wavenumbers_test,
+    integrator,
+    dt,
+    convection_bool = True,
+)
