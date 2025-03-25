60fb6b9eac0560a409f114cc93ea156fc58663bf
Kieran <34337935+KieranSQ@users.noreply.github.com>
2025-03-25 16:20:56 +0100

    updated test

--- test/OperatorsTests.jl
+++ test/OperatorsTests.jl
@@ -14,6 +14,8 @@ using EnrichedFiniteElements
         omega = [0.0, 0.0]
         lower_bounds = [0.0, 0.0, 0.0]
         upper_bounds = [1.0, 1.0, 1.0]
+        t0 = 0
+        triangle_area = 1.0
 
         expected_result1 = zeros(ComplexF64, 3, 3)  # 3x3 matrix of complex zeros
         result1, _ = EnrichedFiniteElements.Operators.pDtq(
@@ -23,8 +25,9 @@ using EnrichedFiniteElements
             B,
             C,
             omega,
+            t0,
+            triangle_area,
         )
-
         @test isapprox(real(result1), real(expected_result1), atol = 1e-6)
         @test isapprox(imag(result1), imag(expected_result1), atol = 1e-6)
     end
@@ -34,12 +37,14 @@ using EnrichedFiniteElements
         B = 0.2
         C = 0.8
         omega = [3.0, -3]
+        t0 = 0.0
+        triangle_area = 1.0
         lower_bounds = [0.0, 0.0, 0.0]
         upper_bounds = [1.0, 1.0, 1.0]
         expected_result2 = ComplexF64[
-            -3.7996281927039286e-6+0.00018323667891961633im -5.370289355812572e-5+0.0009135553137364832im -9.433183249596887e-6+0.0003664505797012257im
-            -5.3702893558125525e-5+0.0009135553137364834im -0.0009903075835568681+0.007978629788904497im -0.00018308044225269775+0.002739782617397888im
-            -9.433183249596893e-6+0.00036645057970122554im -0.00018308044225269813+0.002739782617397888im -4.4936269655183895e-5+0.0012823678089615273im
+            0.00017700008952752238-4.7550877837710987e-5im 0.0008921740882195902-0.00020369759041829422im 0.0003544907357522281-9.333450902768957e-5im
+            0.0008921740882195905-0.00020369759041829466im 0.0079375505383193-0.0012784889010357171im 0.0026818133740891506-0.000589749324279142im
+            0.0003544907357522281-9.333450902768963e-5im 0.002681813374089151-0.0005897493242791426im 0.0012438473568946752-0.00031516696929945756im
         ]
         result2, _ = EnrichedFiniteElements.Operators.pDtq(
             upper_bounds,
@@ -48,7 +53,10 @@ using EnrichedFiniteElements
             B,
             C,
             omega,
+            t0,
+            triangle_area,
         )
+        println(result2)
         @test isapprox(real(result2), real(expected_result2), atol = 1e-6)
         @test isapprox(imag(result2), imag(expected_result2), atol = 1e-6)
 
